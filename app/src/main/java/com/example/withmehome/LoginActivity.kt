a28e09b927517076ba88b93c1c3817df118c7d69
jiwon1031 <yewchung56@naver.com>
2023-02-01 21:35:57 +0900

    로그인

--- app/src/main/java/com/example/withmehome/LoginActivity.kt
+++ app/src/main/java/com/example/withmehome/LoginActivity.kt
@@ -1,40 +1,78 @@
 package com.example.withmehome
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import com.example.withmehome.databinding.ActivityLoginBinding
+import android.widget.ImageButton
+import android.widget.Toast
 import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.AuthErrorCause
 import com.kakao.sdk.user.UserApiClient
 
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        //로그인 공통 콜백 구성
-        val callback: ((OAuthToken?, Throwable?) -> Unit) = { token, error ->
-            if (error != null) { //Login Fail
-                Log.e(TAG, "Kakao Login Failed :", error)
-            } else if (token != null) { //Login Success
-                startActivity(Intent(this, MainActivity::class.java))
+        setContentView(R.layout.activity_login)
+        // 로그인 정보 확인
+        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+            if (error != null) {
+                Toast.makeText(this, "토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
+            }
+            else if (tokenInfo != null) {
+                Toast.makeText(this, "토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MapsActivity::class.java)
+                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                finish()
             }
         }
-
-        binding.kakao.setOnClickListener{
-            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this@LoginActivity)) {
-                UserApiClient.instance.loginWithKakaoTalk(this@LoginActivity, callback = callback)
-            } else {
-                UserApiClient.instance.loginWithKakaoAccount(this@LoginActivity, callback = callback)
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                when {
+                    error.toString() == AuthErrorCause.AccessDenied.toString() -> {
+                        Log.d("[카카오로그인]", "접근이 거부 됨(동의 취소)")
+                    }
+                    error.toString() == AuthErrorCause.InvalidClient.toString() -> {
+                        Log.d("[카카오로그인]", "유효하지 않은 앱")
+                    }
+                    error.toString() == AuthErrorCause.InvalidGrant.toString() -> {
+                        Log.d("[카카오로그인]", "인증 수단이 유효하지 않아 인증할 수 없는 상태")
+                    }
+                    error.toString() == AuthErrorCause.InvalidRequest.toString() -> {
+                        Log.d("[카카오로그인]", "요청 파라미터 오류")
+                    }
+                    error.toString() == AuthErrorCause.InvalidScope.toString() -> {
+                        Log.d("[카카오로그인]", "유효하지 않은 scope ID")
+                    }
+                    error.toString() == AuthErrorCause.Misconfigured.toString() -> {
+                        Log.d("[카카오로그인]", "설정이 올바르지 않음(android key hash)")
+                    }
+                    error.toString() == AuthErrorCause.ServerError.toString() -> {
+                        Log.d("[카카오로그인]", "서버 내부 에러")
+                    }
+                    error.toString() == AuthErrorCause.Unauthorized.toString() -> {
+                        Log.d("[카카오로그인]", "앱이 요청 권한이 없음")
+                    }
+                    else -> { // Unknown
+                        Log.d("[카카오로그인]", "기타 에러")
+                    }
+                }
+            } else if (token != null) {
+                Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MapsActivity::class.java)
+                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                finish()
             }
         }
-    }
-
-    companion object {
-        private const val TAG = "LoginActivity"
+        val kakao_login_button = findViewById<ImageButton>(R.id.login_kakaoButton)
+        // 로그인 버튼
+        kakao_login_button.setOnClickListener {
+            if(UserApiClient.instance.isKakaoTalkLoginAvailable(this)){
+                UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
+            }else{
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
+        }
     }
 }
