d918623d8520068128a22f78a3130fbe2ca24379
GumaLee <rkdxhd97@naver.com>
2023-02-07 19:04:51 +0900

    nickname dup api

--- app/src/main/java/com/example/withmehome/NicknameSetActivity.kt
+++ app/src/main/java/com/example/withmehome/NicknameSetActivity.kt
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import com.example.withmehome.databinding.ActivityNicknameSetBinding
 import kotlinx.android.synthetic.main.activity_nickname_set.*
+import retrofit2.Call
+import retrofit2.Response
 import java.util.regex.Pattern
+import javax.security.auth.callback.Callback
 
 class NicknameSetActivity : AppCompatActivity() {
-    lateinit var viewBinding: ActivityNicknameSetBinding
+    private val binding: ActivityNicknameSetBinding by lazy {
+        ActivityNicknameSetBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBinding = ActivityNicknameSetBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_nickname_set)
 
         // 가입완료 버튼 클릭 시 동네 인증 화면으로 이동
@@ -26,7 +31,7 @@ class NicknameSetActivity : AppCompatActivity() {
         val pwValidation = "^[A-Za-z0-9]{2,10}$"
 
         // 조건부 닉네임 설정
-        viewBinding.edtSetNicknameWriteNickname.addTextChangedListener(object : TextWatcher {
+        binding.edtSetNicknameWriteNickname.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
@@ -39,18 +44,54 @@ class NicknameSetActivity : AppCompatActivity() {
 
             }
         })
+
+        // 액티비티에서 retrofit 사용 시작
+        binding.btnSetNicknameCheckDup.setOnClickListener {
+            retrofitCheckDup()
+        }
     }
 
     // 조건만족 여부에 따른 이벤트
     fun checkPassword():Boolean{
-        val pw = viewBinding.edtSetNicknameWriteNickname.text.toString().trim()
-        val pwCheck = Pattern.matches(viewBinding.edtSetNicknameWriteNickname.toString(), pw)
+        val pw = binding.edtSetNicknameWriteNickname.text.toString().trim()
+        val pwCheck = Pattern.matches(binding.edtSetNicknameWriteNickname.toString(), pw)
         if(pwCheck) {
-            viewBinding.edtSetNicknameWriteNickname.setTextColor(R.color.black.toInt())
+            binding.edtSetNicknameWriteNickname.setTextColor(R.color.black.toInt())
             return true
         } else {
-            viewBinding.edtSetNicknameWriteNickname.setTextColor((R.color.red.toInt()))
+            binding.edtSetNicknameWriteNickname.setTextColor((R.color.red.toInt()))
+            binding.txtNicknameSetExp.text = "2~10자 숫자, 문자만 가능. 특수기호 불가"
             return false
         }
+    }
+
+    // 닉네임 중복 확인
+    private fun retrofitCheckDup() {
+        val service = RetrofitApi.nicknameDupService
+
+        service.getNicknameData(binding.edtSetNicknameWriteNickname.toString())
+            .enqueue(object : retrofit2.Callback<NicknameDupResponse> {
+                override fun onResponse(
+                    call : Call<NicknameDupResponse>,
+                    response: Response<NicknameDupResponse>
+                ) {
+                    if(response.isSuccessful) {
+                        val result = response.body()?.data?.duplicated
+                        if (result == true) {
+                            binding.txtNicknameSetExp.setTextColor(R.color.blue.toInt())
+                            binding.txtNicknameSetExp.text = "사용 가능한 닉네임입니다."
+                            binding.btnSetNicknameComplete.isClickable
+                        }
+                        else {
+                            binding.txtNicknameSetExp.setTextColor(R.color.red.toInt())
+                            binding.txtNicknameSetExp.text = "이미 사용중인 닉네임입니다."
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<NicknameDupResponse>, t: Throwable) {
+                    Log.d("TAG", t.message.toString())
+                }
+            })
     }
 }
