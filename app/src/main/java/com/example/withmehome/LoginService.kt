b36a2a9d0020e2d42afd9806c6c08ff1b3c2c8b1
jiwon1031 <yewchung56@naver.com>
2023-02-12 14:59:49 +0900

    화이팅

---
+++ app/src/main/java/com/example/withmehome/LoginService.kt
@@ -0,0 +1,11 @@
+package com.example.withmehome
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface LoginService {
+    @GET("/api/auth/login")
+    fun getLoginData(@Query("User") user : String) : Call<LoginUserResponse>
+
+}
