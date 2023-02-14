0d72c914faf986747717d580f17f20a6ee474bcb
KellyKimHyeJin <kelly317kim@naver.com>
2023-02-15 06:05:22 +0900

    후기 모아보기 수정

--- app/src/main/java/com/example/withmehome/Review1Fragment.kt
+++ app/src/main/java/com/example/withmehome/Review1Fragment.kt
@@ -17,20 +17,16 @@ class Review1Fragment:Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val list = mutableListOf<ReviewUserdata>()
+        list.add(ReviewUserdata("하나둘","똑똑하시고 성격도 너무 좋으신 분... 궁금한 것 많이 여쭤봤는데 항상 친절하게 잘 설명해주셨어요","하나은행 코테 준비","1월 8일"))
+        list.add(ReviewUserdata("슬랑","수고 많으셨어요!! 영어랑 친해지고 유익한 시간이었어요.","영어회화 스터디 하실 분","1월 7일"))
         binding = FragmentReview1Binding.inflate(inflater, container,false)
-        adapter = ReviewRecyclerItemAdapter(loadData())
+        adapter = ReviewRecyclerItemAdapter(list)
         adapter.setHasStableIds(true)
         binding!!.recyclerViewreview1.adapter = adapter
         binding!!.recyclerViewreview1.layoutManager = LinearLayoutManager(activity)
 
         return binding.root
-    }
-    fun loadData(): List<ReviewUserdata>{
-        val list = mutableListOf<ReviewUserdata>()
-        for(i in 0..4){
-            val data = ReviewUserdata("사용자 이름","내용이 이곳에 표시됩니다.","스터디명","00월 00일")
-            list.add(data)
-        }
-        return list
     }
 }
--- app/src/main/java/com/example/withmehome/Review2Fragment.kt
+++ app/src/main/java/com/example/withmehome/Review2Fragment.kt
@@ -17,20 +17,16 @@ class Review2Fragment:Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val list = mutableListOf<ReviewUserdata>()
+        list.add(ReviewUserdata("하나둘","정말 열심히 하시는 게 눈에 보이시던 분...!! 너무 멋있으셨어요. ","하나은행 코테 준비","1월 8일"))
+        list.add(ReviewUserdata("슬랑","수고 많으셨어요!! 너무 재밌었습니다!! 공부도 열심히 하시고 좋으신 분이였습니다!!","영어회화 스터디 하실 분","1월 7일"))
         binding = FragmentReview2Binding.inflate(inflater, container,false)
-        adapter = ReviewRecyclerItemAdapter2(loadData())
+        adapter = ReviewRecyclerItemAdapter2(list)
         adapter.setHasStableIds(true)
         binding!!.recyclerViewreview2.adapter = adapter
         binding!!.recyclerViewreview2.layoutManager = LinearLayoutManager(activity)
 
         return binding.root
     }
-    fun loadData(): List<ReviewUserdata>{
-        val list = mutableListOf<ReviewUserdata>()
-        for(i in 0..4){
-            val data = ReviewUserdata("사용자 이름","내용이 이곳에 표시됩니다.","스터디명","00월 00일")
-            list.add(data)
-        }
-        return list
-    }
+
 }
